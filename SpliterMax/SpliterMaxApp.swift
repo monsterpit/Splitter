a48dfbad323447cb83be1041f042dcca2c20a5d7
Vikas <vikassalian@icloud.com>
2024-10-27 19:54:40 +0530

    add xcodegen base setup

---
+++ SpliterMax/SpliterMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpliterMaxApp.swift
+//  SpliterMax
+//
+//  Created by Vikas Salian on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SpliterMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
